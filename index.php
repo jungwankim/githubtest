6d0945422386b8cadd461657c9c87461503f7868
jkwan745 <jkwan745@uws64-250.cafe24.com>
2017-07-20 14:04:43 +0900

    test

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    ob_start();
+    session_start();
+    require_once 'include/dbcont.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="author" content="wan">
+        <meta name="robots" content="noindex">
+        <title> personal text viewer</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.js"></script>
+    </head>
+    <body>
+
+            <p>
+               
+            </p>
+      
+    </body>
+</html>
